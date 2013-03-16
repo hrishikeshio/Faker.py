d98f6b1b282f66d938465ce826c722acab984249
hrishikeshio <hrishikesh911@gmail.com>
2013-03-16 23:09:36 +0530

    Added setup. Emptied __init__

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Faker.py',
+      version='1.0',
+      description='Fake data generator for python',
+      author='Hrishikesh Huilgolkar',
+      author_email='hrishikesh911@gmail.com',
+      url='https://github.com/hrishikeshio/Faker.py',
+      packages=['faker'],
+     )
